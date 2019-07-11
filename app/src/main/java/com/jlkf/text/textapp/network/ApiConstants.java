505bfd5ce4c445aa764dc9e1bb428683a873ad5b
xionglang <976301251@qq.com>
2019-07-11 21:12:49 +0800

    更改

--- app/src/main/java/com/jlkf/text/textapp/network/ApiConstants.java
+++ app/src/main/java/com/jlkf/text/textapp/network/ApiConstants.java
@@ -1,12 +1,6 @@
 package com.jlkf.text.textapp.network;
 
-/**
- * desc:
- * author: wecent .
- * date: 2017/9/2 .
- */
+
 public class ApiConstants {
-    public static final String BaseUrl = "http://39.108.91.85:5000/";
-
-
+    public static final String BaseUrl = "http://xx.xx.xx.xx:xxxx/";
 }
